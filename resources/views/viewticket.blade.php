a0f36fe9ff1cb419061e8d9e6b9909357ff5ae05
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-01 12:23:05 +0700

    ngetes telegram authorizatin

--- resources/views/viewticket.blade.php
+++ resources/views/viewticket.blade.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html>
-<head><title>Widget Test</title></head>
+<head>
+  <title>Test Telegram Widget</title>
+</head>
 <body>
-  <div id="telegram-login-widget"></div>
+  <h1>Test Telegram Widget</h1>
   <script>
     function onTelegramAuth(user) {
-      alert('User: ' + JSON.stringify(user));
+      alert('Telegram: ' + JSON.stringify(user));
     }
-    document.addEventListener('DOMContentLoaded', function() {
-      var tgWidget = document.createElement('script');
-      tgWidget.async = true;
-      tgWidget.src = 'https://telegram.org/js/telegram-widget.js?7';
-      tgWidget.setAttribute('data-telegram-login', 'kos74_bot');
-      tgWidget.setAttribute('data-size', 'large');
-      tgWidget.setAttribute('data-userpic', 'false');
-      tgWidget.setAttribute('data-request-access', 'write');
-      tgWidget.setAttribute('data-on-auth', 'onTelegramAuth');
-      document.getElementById('telegram-login-widget').appendChild(tgWidget);
-    });
   </script>
+  <script async src="https://telegram.org/js/telegram-widget.js?7"
+    data-telegram-login="kos74_bot"
+    data-size="large"
+    data-userpic="false"
+    data-request-access="write"
+    data-on-auth="onTelegramAuth"></script>
 </body>
 </html>
