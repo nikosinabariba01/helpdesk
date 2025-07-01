c86fdcc7dacfca9ab4387585f8e2629ab5b1fd7a
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-01 14:02:33 +0700

    ngetes telegram authorizatin

--- resources/views/viewticket.blade.php
+++ resources/views/viewticket.blade.php
@@ -1,23 +1,17 @@
-<div id="telegram-widget-box"></div>
+<div id="telegram-widget-box">
+  <script async src="https://telegram.org/js/telegram-widget.js?22"
+    data-telegram-login="kos74_bot"
+    data-size="large"
+    data-userpic="false"
+    data-request-access="write"
+    data-onauth="onTelegramAuth">
+  </script>
+</div>
 <div id="hasil-telegram"></div>
 
 <script>
-  // Render widget ke dalam #telegram-widget-box (langsung pada page load)
-  document.getElementById('telegram-widget-box').innerHTML = `
-    <script async src="https://telegram.org/js/telegram-widget.js?22"
-      data-telegram-login="kos74_bot"
-      data-size="large"
-      data-userpic="false"
-      data-request-access="write"
-      data-onauth="onTelegramAuth">
-    <\/script>
-  `;
-
-  // Callback HANYA dipanggil SETELAH login berhasil (otomatis oleh Telegram)
   function onTelegramAuth(user) {
-    // Hapus widget
     document.getElementById('telegram-widget-box').innerHTML = "";
-    // Tampilkan data Telegram user
     document.getElementById('hasil-telegram').innerHTML = `
       <b>Halo, ${user.first_name}</b><br>
       Username: @${user.username}<br>
