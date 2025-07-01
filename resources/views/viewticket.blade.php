0fcf82cd7420ea859f5b93b78fa0e4e7aaee30c3
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-01 13:47:35 +0700

    ngetes telegram authorizatin

--- resources/views/viewticket.blade.php
+++ resources/views/viewticket.blade.php
@@ -1,31 +1,31 @@
-<div class="d-flex flex-column align-items-center justify-content-center" style="height: 70vh;">
-    <div class="card shadow-sm p-4" style="min-width: 350px;">
-        <h3 class="mb-4 text-center">Login dengan Telegram</h3>
-        <div id="telegram-widget-box" class="mb-3">
-            <!-- Widget -->
-            <script async src="https://telegram.org/js/telegram-widget.js?22"
-              data-telegram-login="kos74_bot"
-              data-size="large"
-              data-userpic="true"
-              data-request-access="write"
-              data-onauth="onTelegramAuth">
-            </script>
-        </div>
-        <div id="hasil-telegram"></div>
+@extends('mainlayout.layout')
+
+@section('container')
+<div class="d-flex align-items-center justify-content-center" style="min-height: 80vh; background: #f3f6fa;">
+  <div class="card shadow-sm p-5 text-center" style="min-width: 350px;">
+    <h3 class="mb-4">Login dengan Telegram</h3>
+    <div id="telegram-widget-box" class="mb-3">
+      <script async src="https://telegram.org/js/telegram-widget.js?22"
+        data-telegram-login="kos74_bot"
+        data-size="large"
+        data-userpic="true"
+        data-request-access="write"
+        data-onauth="onTelegramAuth">
+      </script>
     </div>
+    <div id="hasil-telegram"></div>
+  </div>
 </div>
 <script>
-  // Penting: pastikan function global!
   window.onTelegramAuth = function(user) {
     document.getElementById('hasil-telegram').innerHTML = `
-      <div class="text-center">
-        <h5>Hello, ${user.first_name || ''} ${user.last_name || ''}!</h5>
-        <img src="${user.photo_url}" class="rounded-circle mb-2" style="width:96px;height:96px">
+      <div class="text-center mt-3">
+        <h5>Halo, ${user.first_name || ''} ${user.last_name || ''}!</h5>
+        ${user.photo_url ? `<img src="${user.photo_url}" class="rounded-circle mb-2" style="width:80px;height:80px">` : ''}
         <div class="mb-1"><b>Username:</b> @${user.username || '-'}</div>
         <div class="mb-1"><b>ID:</b> ${user.id}</div>
       </div>
     `;
-    // Opsional: log ke console
-    console.log('Data Telegram:', user);
   }
 </script>
+@endsection
