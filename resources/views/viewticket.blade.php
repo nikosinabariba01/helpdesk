b5046296904224e8deee99601a4d0d7db8359da2
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-01 14:39:40 +0700

    ngetes telegram authorizatin

--- resources/views/viewticket.blade.php
+++ resources/views/viewticket.blade.php
@@ -1,16 +1,34 @@
-<!-- resources/views/test-telegram.blade.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Test Telegram Widget</title>
-</head>
-<body>
-  <h2>Test Login Telegram</h2>
+<div id="telegram-widget-box">
   <script async src="https://telegram.org/js/telegram-widget.js?22"
-    data-telegram-login="kos74_bot"
+    data-telegram-login="kos74_bot"  {{-- Ganti dengan username bot kamu --}}
     data-size="large"
-    data-auth-url="/telegram/callback"
-    data-request-access="write">
+    data-userpic="false"
+    data-request-access="write"
+    data-onauth="onTelegramAuth">
   </script>
-</body>
-</html>
+</div>
+
+<div id="hasil-telegram"></div>
+
+<script>
+function onTelegramAuth(user) {
+  document.getElementById('hasil-telegram').innerHTML = `
+    <b>Data dari Telegram (client):</b>
+    <pre>${JSON.stringify(user, null, 2)}</pre>
+    <b>Kirim ke server untuk di-echo:</b>
+  `;
+  // Kirim ke Laravel (tes echo)
+  fetch('/tes-echo-telegram', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-TOKEN': '{{ csrf_token() }}'
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res => res.text())
+  .then(res => {
+    document.getElementById('hasil-telegram').innerHTML += res;
+  });
+}
+</script>
