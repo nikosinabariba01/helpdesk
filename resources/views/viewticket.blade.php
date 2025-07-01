3b8c0675593c89e87f483139c821714ad2765a27
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-01 14:24:31 +0700

    ngetes telegram authorizatin

--- resources/views/viewticket.blade.php
+++ resources/views/viewticket.blade.php
@@ -2,24 +2,23 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Cek Data Telegram Widget</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test Telegram Widget</title>
 </head>
 <body>
   <h2>Login dengan Telegram</h2>
   <div id="telegram-widget-box">
+    <!-- HANYA ini di sini -->
     <script async src="https://telegram.org/js/telegram-widget.js?22"
-      data-telegram-login="kos74_bot"      <!-- Ganti dengan username botmu tanpa @ -->
+      data-telegram-login="kos74_bot"
       data-size="large"
       data-userpic="false"
       data-request-access="write"
       data-onauth="onTelegramAuth">
     </script>
   </div>
-  
+  <!-- CALLBACK HARUS DILUAR SITU! -->
   <script>
     function onTelegramAuth(user) {
-      // Tampilkan semua data user ke console browser
       console.log('DATA DARI TELEGRAM:', user);
     }
   </script>
