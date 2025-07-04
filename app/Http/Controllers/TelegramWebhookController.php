1c07e292f1946efc5b6366dab011bea92c2b7522
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-04 10:06:45 +0700

    menambahkan inline keyboard untuk memetakan id_ticket yang dikirim penyewa masi 1

--- app/Http/Controllers/TelegramWebhookController.php
+++ app/Http/Controllers/TelegramWebhookController.php
@@ -1,14 +1,18 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use App\Models\Ticket;
 use App\Models\User;
+use App\Services\TelegramService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
-class TelegramWebhookController extends Controller {
-    public function handle(Request $request) {
+class TelegramWebhookController extends Controller
+{
+    public function handle(Request $request)
+    {
         // Menangani Update (Pesan atau Callback Query)
         $update = $request->all();
         Log::info('Webhook DITERIMA:', $update);
@@ -26,9 +30,10 @@ class TelegramWebhookController extends Controller {
     }
 
     // Menangani callback query
-    public function handleCallback($callbackQuery) {
+    public function handleCallback($callbackQuery)
+    {
         $chatId = $callbackQuery['from']['id']; // Gunakan callback_query['from']['id'] untuk mendapatkan chat_id
-        $data   = $callbackQuery['data'];       // Data yang dikirimkan saat memilih tiket, seperti "ticket_1234"
+        $data = $callbackQuery['data']; // Data yang dikirimkan saat memilih tiket, seperti "ticket_35"
 
         Log::info('Callback query diterima dengan data: ' . $data); // Debugging log
 
@@ -38,15 +43,21 @@ class TelegramWebhookController extends Controller {
             $ticket = Ticket::find($ticketId);
             if ($ticket) {
                 // Kirimkan pesan kepada pengguna untuk mengirimkan komentar
+                Log::info("Tiket ditemukan: {$ticket->id} - {$ticket->subject}");
                 $this->sendTelegramMessage($chatId, "Silakan kirim komentar Anda untuk tiket #{$ticket->id} - {$ticket->subject}:");
             } else {
+                Log::error("Tiket dengan ID {$ticketId} tidak ditemukan.");
                 $this->sendTelegramMessage($chatId, "Tiket yang Anda pilih tidak ditemukan.");
             }
+        } else {
+            Log::error("Data callback query tidak valid: {$data}");
         }
     }
 
+
     // Menangani pesan biasa
-    public function handleMessage($message) {
+    public function handleMessage($message)
+    {
         $telegramUserId   = $message['from']['id'];
         $telegramUsername = $message['from']['first_name'] . ' ' . $message['from']['last_name'];
         $telegramChatId   = $message['chat']['id'];
@@ -73,20 +84,18 @@ class TelegramWebhookController extends Controller {
         return response()->json(['ok' => true]);
     }
 
-    protected function sendTelegramMessage($chatId, $message) {
+    protected function sendTelegramMessage($chatId, $message)
+    {
         // Kirim pesan ke Telegram
         $telegram = new TelegramService();
         $telegram->sendMessage($chatId, $message);
     }
 
-    protected function getTicketIdForUser($user) {
+    protected function getTicketIdForUser($user)
+    {
         // Ambil tiket yang valid berdasarkan user
         // Misalnya ambil tiket pertama yang valid
         $ticket = Ticket::where('user_id', $user->id)->first();
         return $ticket ? $ticket->id : null;
     }
 }
-
-
-
-
