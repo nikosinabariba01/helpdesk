a4acf058eb9f92cb686eb1be7d62c49630918419
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-25 20:50:24 +0700

    perbaiki side bar viewticket

--- resources/views/mainlayout/navbar/nav6.blade.php
+++ resources/views/mainlayout/navbar/nav6.blade.php
@@ -1,7 +1,8 @@
-<div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+<div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+    @if(Auth::user()->role == 'penyewa')
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link " href="{{route('customer.index')}}">
+          <a class="nav-link " href="{{ route('customer.index') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
             </div>
@@ -9,7 +10,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="{{route('customer.tickets')}}">
+          <a class="nav-link " href="{{ route('customer.tickets') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
             </div>
@@ -17,7 +18,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="{{route('customer.viewprocess')}}">
+          <a class="nav-link " href="{{ route('customer.viewprocess') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
             </div>
@@ -25,7 +26,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="{{route('customer.profile')}}">
+          <a class="nav-link " href="{{ route('customer.profile') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-app text-info text-sm opacity-10"></i>
             </div>
@@ -33,4 +34,67 @@
           </a>
         </li>
       </ul>
-    </div>
+    @elseif(Auth::user()->role == 'pengurus' || Auth::user()->role == 'pemilik')
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('teknisi.index') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('teknisi.viewasigne') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">My Assigned Ticket</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('teknisi.closeticket') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">My Closed Ticket</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('teknisi.ListTicket') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-app text-info text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Ticket List</span>
+                </a>
+            </li>
+        </ul> 
+    @elseif(Auth::user()->role == 'admin')
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.dashboard') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Admin Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.manageusers') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-users text-success text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Manage Users</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.settings') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-settings text-warning text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Settings</span>
+                </a>
+            </li>
+        </ul>
+    @endif
+</div>
