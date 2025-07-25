30ce02cc5d30bd4f748f6f75a1309aa013744032
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-26 06:13:51 +0700

    ganti icon escalation

--- resources/views/mainlayout/navbar/teknav3.blade.php
+++ resources/views/mainlayout/navbar/teknav3.blade.php
@@ -12,7 +12,7 @@
         <li class="nav-item">
           <a class="nav-link" href="{{route('tickets.viewEscalation')}}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="ni retweet text-warning text-sm opacity-10"></i>
+              <i class="ni ni-android-sort text-warning text-sm opacity-10"></i>
             </div>
             <span class="nav-link-text ms-1">Escalation Queue</span>
           </a>
