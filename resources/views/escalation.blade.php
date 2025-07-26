5d25d56b1a9900a961f2de986e780274bec3ac42
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-27 04:23:31 +0700

    menambahkan tombol search dan sorting ke table escalation untuk uji coba 4

--- resources/views/escalation.blade.php
+++ resources/views/escalation.blade.php
@@ -45,7 +45,7 @@
             </div>
             @else
             <div class="table-responsive margin-right: 15px;" style="height: 400px; max-height: 400px; overflow-y: auto;">
-                <table class="table table-hover align-items-center mb-0" id="escalationTable">
+                <table class="table table-hover align-items-center mb-0">
                     <thead>
                         <tr>
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center" style="padding: 10px;">subject</th>
