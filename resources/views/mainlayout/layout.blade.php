30e8d3f206d96b67a29b1ce00f347ad91bc2f5c0
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-27 02:07:30 +0700

    menambahkan tombol search dan sorting ke table escalation untuk uji coba 2

--- resources/views/mainlayout/layout.blade.php
+++ resources/views/mainlayout/layout.blade.php
@@ -23,13 +23,6 @@
   <title>
     Service IT
   </title>
-  <!-- Tambahkan CDN jQuery dan DataTables -->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-
-
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
@@ -130,26 +123,6 @@
       Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
     }
   </script>
-
-  <script>
-    $(document).ready(function() {
-      // Inisialisasi DataTable dengan gaya Bootstrap
-      $('.table').DataTable({
-        paging: true, // Mengaktifkan pagination
-        searching: true, // Mengaktifkan pencarian
-        ordering: true, // Mengaktifkan sorting
-        order: [
-          [0, 'asc']
-        ], // Urutan default (misalnya, kolom pertama secara ascending)
-        responsive: true, // Membuat tabel responsif
-        language: {
-          search: "_INPUT_", // Mengubah placeholder pencarian
-          searchPlaceholder: "Cari tiket..."
-        }
-      });
-    });
-  </script>
-
 
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
