06d0ccf3df44592ba58e6224ed99da025bce2d32
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-27 01:25:55 +0700

    menambahkan cdn jquery dan datatables di mainlayout

--- resources/views/mainlayout/layout.blade.php
+++ resources/views/mainlayout/layout.blade.php
@@ -23,6 +23,11 @@
   <title>
     Service IT
   </title>
+  <!-- Tambahkan CDN jQuery dan DataTables -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
