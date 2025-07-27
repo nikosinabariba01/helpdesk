8a0a42145e7f7aac5ef7ac02d55a187d5747d8b9
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-27 22:00:59 +0700

    memperbaiki gambar profil yang tidak tampil

--- resources/views/escalation.blade.php
+++ resources/views/escalation.blade.php
@@ -49,7 +49,7 @@
                     <tbody>
                         @foreach($teknisi_data_ticket as $teknisidataticket)
                         <tr>
-                            <td class="align-middle text-center text-sm border border-light">
+                            <td class="align-middle text-sm border border-light">
                                 <div class="d-flex px-2 py-1">
                                     <div class="d-flex flex-column justify-content-center">
                                         <h6 class="mb-0 text-s text-limit-35" title="Subject">
