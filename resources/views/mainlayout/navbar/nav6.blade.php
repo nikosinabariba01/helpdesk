474e5276a0d7e9af6eb08b90209288f7435bfd7d
nikosinabariba01 <geraldiniko@Gmail.com>
2025-07-29 19:52:58 +0700

    memperbaiki path photo profiles agar aman

--- resources/views/mainlayout/navbar/nav6.blade.php
+++ resources/views/mainlayout/navbar/nav6.blade.php
@@ -80,7 +80,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('admin.manageusers') }}">
+                <a class="nav-link" href="{{ route('admin.manageuser') }}">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-users text-success text-sm opacity-10"></i>
                     </div>
