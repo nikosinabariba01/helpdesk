f82e21860fc7878f2d0d540198780d108d595cea
nikosinabariba01 <geraldiniko@Gmail.com>
2025-12-21 05:08:20 +0700

    memperbaiki tampilan viewticket4

--- resources/views/viewticket.blade.php
+++ resources/views/viewticket.blade.php
@@ -124,7 +124,7 @@
           <div class="d-flex flex-column">
             <h6 class="mb-2 text-dark font-weight-bold text-sm">{{ $userName }}</h6>
             <span class="text-xs">
-              {{ 'sp-' . substr($ticket->id, 0, 3) . '-' . $ticket->uuid . '-' . \Carbon\Carbon::parse($ticket->created_at)->format('dmy') . '-' . ($ticket->Jenis_Pengaduan == 0 ? '0' : '1') }}
+              {{ 'sp-' . substr($ticket->id, 0, 3) . \Carbon\Carbon::parse($ticket->created_at)->format('dmy') . ($ticket->Jenis_Pengaduan == 0 ? '0' : '1') }}
             </span>
           </div>
         </div>
