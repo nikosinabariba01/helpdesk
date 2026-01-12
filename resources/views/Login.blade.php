24de47f791693f51ba5f802908a3f73a5512f3e9
nikosinabariba01 <geraldiniko@Gmail.com>
2026-01-12 08:32:05 +0700

    ganti tampilan login

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -93,7 +93,7 @@
                         </div>
                         <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="
-                    background-image: url('style/assets/img/maxresdefault.jpg');
+                    background-image: url('style/assets/img/teraskos.png');
                     background-size: cover;
                   ">
                                 <span class="mask bg-gradient-primary opacity-6"></span>
