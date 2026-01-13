5fbb2c2cacacf85ef8b4a4570700caa0e68d04ec
nikosinabariba01 <geraldiniko@Gmail.com>
2026-01-13 20:47:04 +0700

    memperbaiki table custumer

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -117,14 +117,14 @@
           </div>
         </div>
       </div>
-      <div class="card-body px-0 pt-0 pb-2 h-500">
+      <div class="card-body px-0 pt-0 pb-0">
         @if($data_ticket->isEmpty())
-        <div class="table-responsive margin-right: 15px; position: relative;" style="height: 400px; max-height: 400px; overflow-y: auto;">
+        <div class="table-responsive" style="height: 400px; max-height: 400px; overflow-y: auto;">
           <!-- Add your button here -->
           <a href="{{ route('customer.tickets') }}" class="btn btn-primary position-absolute top-50 start-50 translate-middle">Buat Tiket</a>
         </div>
         @else
-        <div class="table-responsive margin-right: 15px;" style="height: 400px; max-height: 400px; overflow-y: auto;">
+        <div class="table-responsive" style="height: auto; max-height: 400px; overflow-y: auto;">
           <table class="table align-items-center mb-0 " id="TicketTable">
             <thead>
               <tr>
