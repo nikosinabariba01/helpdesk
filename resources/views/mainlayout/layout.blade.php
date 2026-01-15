97a1483ecd99f1f43d13f01c38b81ecfe4d6a96a
nikosinabariba01 <geraldiniko@Gmail.com>
2026-01-15 20:47:31 +0700

    memperbaiki view create pengumuman(choices dan layoutnya)

--- resources/views/mainlayout/layout.blade.php
+++ resources/views/mainlayout/layout.blade.php
@@ -42,7 +42,7 @@
   <!-- CSS Files -->
   <link id="pagestyle" href="{{ asset('style/assets/css/argon-dashboard.css') }}" rel="stylesheet">
 <!-- Choices.js CSS -->
-<link href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/base.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/choices.js@11.1.0/public/assets/styles/choices.min.css" rel="stylesheet">
 
 <!-- Choices.js JS -->
 <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
