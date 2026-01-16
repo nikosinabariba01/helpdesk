8dd2e82e4177d5f3f87e47f2993e178a50b8263e
nikosinabariba01 <geraldiniko@Gmail.com>
2026-01-16 12:20:58 +0700

    memperbaiki sidenav managepengumuman

--- resources/views/mainlayout/navbar/teknav.blade.php
+++ resources/views/mainlayout/navbar/teknav.blade.php
@@ -2,7 +2,7 @@
   @if(Auth::user()->role == 'pemilik')
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link active" href="{{route('teknisi.index')}}">
+      <a class="nav-link {{ Route::currentRouteName() == 'teknisi.index' ? 'active' : '' }}" href="{{route('teknisi.index')}}">
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
         </div>
@@ -42,7 +42,7 @@
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="{{route('pengumuman.index')}}">
+      <a class="nav-link {{ Route::currentRouteName() == 'pengumuman.index' ? 'active' : '' }}" href="{{route('pengumuman.index')}}">
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-collection text-secondary text-sm opacity-10"></i>
         </div>
@@ -53,7 +53,7 @@
   @elseif(Auth::user()->role == 'pengurus')
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link active" href="{{route('teknisi.index')}}">
+      <a class="nav-link {{ Route::currentRouteName() == 'teknisi.index' ? 'active' : '' }}" href="{{route('teknisi.index')}}">
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
         </div>
@@ -85,7 +85,7 @@
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="{{route('pengumuman.index')}}">
+      <a class="nav-link {{ Route::currentRouteName() == 'pengumuman.index' ? 'active' : '' }}" href="{{route('pengumuman.index')}}">
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-collection text-secondary text-sm opacity-10"></i>
         </div>
