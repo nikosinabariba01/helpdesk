efb381eb4b962a771376ad4a9f99cdcfebd3a673
nikosinabariba01 <geraldiniko@Gmail.com>
2026-01-22 20:28:00 +0700

    modfikasi telegram auth untuk ambil foto profile jika belum ada foto profilnya

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::post('/tickets/{ticket}/comments', [TicketCommentController::class, 'store'])->name('comments.store')->middleware('userAkses:penyewa,admin');;
     Route::post('/tickets/{ticket}/teknisicomments', [TicketCommentController::class, 'teknisiComment'])->name('comments.teknisiComment')->middleware('userAkses:pengurus,pemilik,admin');
-    Route::match(['GET', 'POST'], '/telegram/auth/{ticket_id}', [TelegramAuthController::class, 'telegramAuthorize'])->name('telegram.auth');
+    // Route::match(['GET', 'POST'], '/telegram/auth/{ticket_id}', [TelegramAuthController::class, 'telegramAuthorize'])->name('telegram.auth');
     Route::match(['GET', 'POST'], '/telegram/auth/profile', [TelegramAuthController::class, 'telegramAuthorizeFromProfile'])->name('telegram.auth.profile');
     Route::post('/telegram/logout', [TelegramAuthController::class, 'telegramLogout'])->name('telegram.logout');
 
