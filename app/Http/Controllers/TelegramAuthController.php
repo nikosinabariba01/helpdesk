7c55b66ab656f633c94abbc695f52523664c4561
nikosinabariba01 <geraldiniko@Gmail.com>
2026-01-22 19:59:29 +0700

    modfikasi telegram auth untuk ambil foto profile jika belum ada foto profilnya

--- app/Http/Controllers/TelegramAuthController.php
+++ app/Http/Controllers/TelegramAuthController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Route;
+
 
 
 class TelegramAuthController extends Controller
