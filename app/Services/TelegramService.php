ad46fe926d7aa6445a5f7d21294d5b67bfcee34d
nikosinabariba01 <geraldiniko@Gmail.com>
2026-02-05 09:21:39 +0700

    ngetes krn bug telegram ubah route telegram, kembalikan ke function testing apakah telegram ngerespon chat dan mengirimkan pesan helo

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -1,44 +1,49 @@
 <?php
+
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
-class TelegramService {
+class TelegramService
+{
     private $apiUrl;
     private $botToken;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->botToken = config('services.telegram.bot_token'); // Ambil bot token dari konfigurasi
         $this->apiUrl   = "https://api.telegram.org/bot{$this->botToken}/";
     }
 
-    public function sendMessage($chatId, $message, $keyboard = null) {
+    public function sendMessage($chatId, $message, $keyboard = null)
+    {
         $url  = $this->apiUrl . 'sendMessage';
         $data = [
             'chat_id'    => $chatId,
             'text'       => $message,
             'parse_mode' => 'HTML',
         ];
-    
+
         if ($keyboard) {
             $data['reply_markup'] = json_encode($keyboard);
         }
-    
+
         // Log data yang akan dikirim ke Telegram
         Log::info("URL: " . $url);
         Log::info("Data yang dikirim: " . json_encode($data));
-    
+
         // Kirim permintaan HTTP ke API Telegram
         $response = Http::post($url, $data);
-    
-        // Cek dan log respons dari Telegram
+
         if ($response->successful()) {
             Log::info("Pesan berhasil dikirim ke Telegram.");
-        } else {
-            Log::error("Terjadi kesalahan saat mengirim pesan ke Telegram. Respons: " . $response->body());
+        } elseif ($response->failed()) {
+            Log::error("Gagal mengirim pesan ke Telegram. Respons: " . $response->body());
+        } elseif ($response->clientError()) {
+            Log::error("Client error saat mengirim pesan ke Telegram. Status Code: " . $response->status());
+        } elseif ($response->serverError()) {
+            Log::error("Server error saat mengirim pesan ke Telegram. Status Code: " . $response->status());
         }
     }
-    
 }
-
