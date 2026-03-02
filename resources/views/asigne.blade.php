762d984ec4f49ec7dbfb2dc271c60e73efa9e174
nikosinabariba01 <geraldiniko@Gmail.com>
2026-03-02 17:10:29 +0700

    memindahkan tombol close ke kolom aksi status

--- resources/views/asigne.blade.php
+++ resources/views/asigne.blade.php
@@ -115,7 +115,7 @@
                                     <button type="submit" class="btn btn-sm btn-outline-danger btn-transparent text-secondary">Cancel Process</button>
                                 </form>
                                 <!-- Tombol Request Follow-up jika tiket belum escalated -->
-                                <form method="POST" action="{{ $teknisidataticket->status == 'escalated' ? route('ticketsteknisi.cancelRequestFollowUp', $teknisidataticket->id) : route('ticketsteknisi.requestFollowup', $teknisidataticket->id) }}">
+                                <form method="POST" action="{{ $teknisidataticket->status == 'escalated' ? route('ticketsteknisi.cancelRequestFollowUp', $teknisidataticket->id) : route('ticketsteknisi.requestFollowup', $teknisidataticket->id) }}" class="mb-2">
                                     @csrf
                                     @if($teknisidataticket->status == 'escalated')
                                     @method('PUT') <!-- Menggunakan PUT karena kita akan memperbarui status tiket -->
@@ -128,6 +128,14 @@
                                     </button>
                                     @endif
                                 </form>
+                                <!-- Tombol Close -->
+                                <form method="POST" action="{{ route('ticketsteknisi.close', $teknisidataticket->id) }}" id="closeTicketForm-{{ $teknisidataticket->id }}">
+                                    @method('PUT')
+                                    @csrf
+                                    <button type="button" class="btn btn-sm btn-outline-danger btn-transparent text-danger" data-bs-toggle="modal" data-bs-target="#modal-confirmation" data-form-id="closeTicketForm-{{ $teknisidataticket->id }}">
+                                        <i class="fa fa-check pe-2 text-danger"></i>Close
+                                    </button>
+                                </form>
                                 @else
                                 <!-- Jika sudah ada pemilik yang meng-assign, hanya tampilkan tombol Cancel Assign -->
                                 <form action="{{ route('tickets.cancel_assign', $teknisidataticket->id) }}" method="POST" class="mb-2">
@@ -135,32 +143,22 @@
                                     @method('PUT')
                                     <button type="submit" class="btn btn-sm btn-outline-danger btn-transparent text-secondary">Cancel Process</button>
                                 </form>
+                                <!-- Tombol Close -->
+                                <form method="POST" action="{{ route('ticketsteknisi.close', $teknisidataticket->id) }}" id="closeTicketForm-{{ $teknisidataticket->id }}">
+                                    @method('PUT')
+                                    @csrf
+                                    <button type="button" class="btn btn-sm btn-outline-danger btn-transparent text-danger" data-bs-toggle="modal" data-bs-target="#modal-confirmation" data-form-id="closeTicketForm-{{ $teknisidataticket->id }}">
+                                        <i class="fa fa-check pe-2 text-danger"></i>Close
+                                    </button>
+                                </form>
                                 @endif
                             </td>
-                            <!-- "Edit" button within a dropdown -->
+                            <!-- Detail button -->
                             <td class="align-middle text-center border border-light">
-                                <div class="dropdown">
-                                    <a class="btn btn-link" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="fa fa-ellipsis-v fa-sm"></i>
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
-                                        <li>
-                                        <li>
-                                            <a class="dropdown-item text-info" href="{{ route('viewticketteknisi.index', ['id' => $teknisidataticket->id]) }}">
-                                                <i class="fa fa-eye pe-2 text-info"></i>Detail
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <form method="POST" action="{{ route('ticketsteknisi.close', $teknisidataticket->id) }}" id="closeTicketForm-{{ $teknisidataticket->id }}">
-                                                @method('PUT')
-                                                @csrf
-                                                <button type="button" class="dropdown-item text-danger" data-bs-toggle="modal" data-bs-target="#modal-confirmation" data-form-id="closeTicketForm-{{ $teknisidataticket->id }}">
-                                                    <i class="fa fa-minus pe-2 text-danger"></i>close
-                                                </button>
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </div>
+                                <a class="dropdown-item"
+                                    href="{{ route('viewticketteknisi.index', ['id' => $teknisidataticket->id]) }}">
+                                    <i class="fa fa-eye pe-2 text-dark"></i>
+                                </a>
                             </td>
                         </tr>
                         @endforeach
