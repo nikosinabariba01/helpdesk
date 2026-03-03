e529aaf6dc75f06ddfe58d5dee005d9014c5d063
nikosinabariba01 <geraldiniko@Gmail.com>
2026-03-03 17:32:45 +0700

    memperbaiki tombol search dan pagination table

--- resources/views/ListTicket.blade.php
+++ resources/views/ListTicket.blade.php
@@ -239,17 +239,16 @@
             paging: false, // We will handle pagination manually
             lengthChange: false,
             info: false,
-            columnDefs: [
-                {
-                    targets: [0, 1, 2],  // Only allow sorting for subject, user, and status columns
-                    orderable: true       // Enable sorting on these columns
+            columnDefs: [{
+                    targets: [0, 1, 2],
+                    orderable: true
                 },
                 {
-                    targets: [3, 4, 5],  // Disable sorting on other columns (deskripsi, aksi status, and action)
-                    orderable: false     // Disable sorting on these columns
+                    targets: [3, 4, 5],
+                    orderable: false
                 }
             ],
-            order: [[3, 'desc']]  // Initially sort by 'created_at' (column 3) descending (latest first)
+            order: [[3, 'desc']] // Initially sort by 'created_at' (column 3) descending
         });
 
         // Set default value for dropdown (display name only, no actual value)
