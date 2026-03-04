023317cc872cd58e9914caa1a553cd41c5b53493
nikosinabariba01 <geraldiniko@Gmail.com>
2026-03-05 02:55:13 +0700

    memperbaiki filter dropdown dan sorting table

--- resources/views/ListTicket.blade.php
+++ resources/views/ListTicket.blade.php
@@ -470,7 +470,7 @@
             // FIX: Jika reset ke Semua, pastikan fallback ke semua data dan show
             if (!hasActiveFilter) {
                 $('#TicketTable tbody tr').show();
-                sortTableByDate(currentSort);
+                sortTableByDate('desc');
                 currentPage = 1;
                 updatePagination();
             }
