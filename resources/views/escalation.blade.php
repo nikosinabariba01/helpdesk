41879e5c73601c24b7e9fe2e2fdbe8c9a590aa51
nikosinabariba01 <geraldiniko@Gmail.com>
2026-03-04 23:07:47 +0700

    memperbaiki warna dan icon dari navbar

--- resources/views/escalation.blade.php
+++ resources/views/escalation.blade.php
@@ -59,11 +59,11 @@
                             <tbody>
                                 @foreach ($teknisi_data_ticket as $teknisidataticket)
                                     <tr class="align-middle text-sm border border-light"
-                                        data-created-at="{{ $dataticket->created_at->timestamp }}"
-                                        data-jenis-pengaduan="{{ $dataticket->Jenis_Pengaduan }}"
-                                        data-status="{{ $dataticket->status }}">
+                                        data-created-at="{{ $teknisidataticket->created_at->timestamp }}"
+                                        data-jenis-pengaduan="{{ $teknisidataticket->Jenis_Pengaduan }}"
+                                        data-status="{{ $teknisidataticket->status }}">
                                         <td class="align-middle text-sm border border-light"
-                                            data-subject="{{ $dataticket->subject }}">
+                                            data-subject="{{ $teknisidataticket->subject }}">
                                             <div class="d-flex px-2 py-1">
                                                 <div class="d-flex flex-column justify-content-center">
                                                     <h6 class="mb-0 text-s text-limit-35" title="Subject">
