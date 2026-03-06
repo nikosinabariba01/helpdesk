42a66b9f10a2cda76e66e4b4e9f0e0eaaa6436c9
nikosinabariba01 <geraldiniko@Gmail.com>
2026-03-06 11:59:52 +0700

    memperbaiki tombol ordering sorting headernya pada table

--- resources/views/ListTicket.blade.php
+++ resources/views/ListTicket.blade.php
@@ -577,7 +577,7 @@
             }
         }
         updatePagination(); // Update pagination after sorting
-        sortTableByColumn(0, 'asc'); // Mengurutkan kolom pertama (misalnya berdasarkan abjad A-Z)
+        sortTableByColumn(2, 'asc'); // Mengurutkan kolom pertama (misalnya berdasarkan abjad A-Z)
         sortTableByDate('desc');
     });
 </script>
