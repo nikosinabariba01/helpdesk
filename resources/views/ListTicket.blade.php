e1958c9871f1c5eae3ff1d372a4be03e1d2553d1
nikosinabariba01 <geraldiniko@Gmail.com>
2026-03-06 09:26:20 +0700

    memperbaiki tombol ordering sorting headernya pada table

--- resources/views/ListTicket.blade.php
+++ resources/views/ListTicket.blade.php
@@ -319,7 +319,7 @@
         var table = $('#TicketTable').DataTable({
             searching: true,
             ordering: true,
-            paging: true, // We will handle pagination manually
+            paging: false, // We will handle pagination manually
             lengthChange: false,
             info: false,
             columnDefs: [{
