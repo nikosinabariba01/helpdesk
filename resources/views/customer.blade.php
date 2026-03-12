0db6dd6163d335a2d3370e9f0116a1901c8c2cff
nikosinabariba01 <geraldiniko@Gmail.com>
2026-03-12 07:19:28 +0700

    coba MODIFIKASI limit karakter subject pada table customer

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -148,10 +148,10 @@
                                             <td class="align-middle text-sm border border-light">
                                                 <div class="d-flex px-2 py-1">
                                                     <div class="d-flex flex-column justify-content-center">
-                                                        <h6 class="mb-0 text-s text-limit-35" title="Subject">
+                                                        <h6 class="mb-0 text-s" title="Subject">
                                                             <a
                                                                 href="{{ route('viewtickets.index', ['id' => $dataticket->id]) }}">
-                                                                {{ $dataticket->subject }}
+                                                                {{Str::limit($dataticket->subject, 35, '...');}}
                                                             </a>
                                                         </h6>
                                                         <div class="d-flex list-inline">
