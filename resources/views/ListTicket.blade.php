3a7f43397a2ec0470bc0c34e243e8e3c52868570
nikosinabariba01 <geraldiniko@Gmail.com>
2026-05-12 21:48:48 +0700

    izin update menu bar agar pengurus ada menu eskalasi

--- resources/views/ListTicket.blade.php
+++ resources/views/ListTicket.blade.php
@@ -19,7 +19,7 @@
 <div class="col-lg-12 mb-lg-0 mb-4 ">
     <div class="card z-index-2 h-100 d-flex flex-column shadow-lg" style="border: 1px solid #e4e4e4;">
         <div class="card-header pb-0 d-flex align-items-center justify-content-between">
-            <h6 class="mb-0">All Ticket List</h6>
+            <h6 class="mb-0">All Ticket Lis</h6>
             <div class="d-flex">
                 <!-- Kolom Pencarian dengan input-group -->
                 <div class="input-group input-group-sm">
