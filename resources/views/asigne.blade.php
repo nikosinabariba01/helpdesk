a7240ab9630d023b3629208aa3293216c72a1e90
nikosinabariba01 <geraldiniko@Gmail.com>
2026-05-12 23:47:07 +0700

    mengubah processing time agar memakai Tanggal_Proses bukan Updated_at

--- resources/views/asigne.blade.php
+++ resources/views/asigne.blade.php
@@ -108,7 +108,7 @@
                                                         <li class="text-xs list-inline-item text-secondary"
                                                             title="Created Date">
                                                             <i class="fa fa-circle fa-xs text-secondary"></i>
-                                                            {{ \Carbon\Carbon::parse($teknisidataticket->created_at)->format('d-m-Y H:i') }}
+                                                            {{ \Carbon\Carbon::parse($teknisidataticket->created_at)->diffForHumans() }}
                                                         </li>
 
                                                         @if ($teknisidataticket->status === 'close' && $teknisidataticket->Tanggal_Selesai)
