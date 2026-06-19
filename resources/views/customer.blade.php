c3ae4f03c7228f8c18a644bee6e5172bcd7471f0
nikosinabariba01 <geraldiniko@Gmail.com>
2026-06-19 16:21:15 +0700

    mengubah sedikit bagian edit, mengubah alamat menjadi no kos

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -452,7 +452,7 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="Lokasi">Alamat</label>
+                                        <label for="Lokasi">Lokasi/Nokamar</label>
                                         <input type="text" id="Lokasi" name="Lokasi"
                                             class="form-control border-input">
                                         @error('Lokasi')
