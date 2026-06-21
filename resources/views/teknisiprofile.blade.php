f65cc8c5facdbc0dff5428ac0101c73a1c29b644
nikosinabariba01 <geraldiniko@Gmail.com>
2026-06-21 15:20:21 +0700

    memperbaiki logo di navbar menu kiri

--- resources/views/teknisiprofile.blade.php
+++ resources/views/teknisiprofile.blade.php
@@ -25,9 +25,9 @@
         <span class="mask bg-primary opacity-6"></span>
     </div>
     <aside
-        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
-        id="sidenav-main">
-        <div class="sidenav-header">
+        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-radius-xl my-3 fixed-start ms-4 shadow-lg"
+        id="sidenav-main" style="border: 2px solid #e4e4e4;">
+        <div class="sidenav-header mb-4">
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
                 aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0 d-flex flex-column align-items-center justify-content-center"
