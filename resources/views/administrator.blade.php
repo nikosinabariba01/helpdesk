376ee7de6281f45aa05411681e2aa8330518c2d8
nikosinabariba01 <geraldiniko@Gmail.com>
2026-06-24 08:25:36 +0700

    memperbaiki jumlah tikcet di dashboard admin agar current ticket menjadi status open

--- resources/views/administrator.blade.php
+++ resources/views/administrator.blade.php
@@ -32,7 +32,7 @@
                     </div>
                 </div>
                 <div class="col-4 text-end">
-                    <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                    <div class="icon icon-shape bg-gradient-success shadow-primary text-center rounded-circle">
                         <i class="fa fa-copy text-lg opacity-10" aria-hidden="true"></i>
                     </div>
                 </div>
